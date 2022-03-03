3dd7843e48aadaa21a5e50fabd3c79eda3bf9f2d
figielf <mfig99@gmail.com>
2022-03-03 12:48:05 +0100

    refactor

--- mllib/applications/optimizers_comparison/momentum_optimizers_comparison.py
+++ mllib/applications/optimizers_comparison/momentum_optimizers_comparison.py
@@ -31,65 +31,12 @@ def _forward_log_odds(x, W, b):
     return x.dot(W) + b
 
 
-class LRfit_by_gd_with_momentum():
-    def __init__(self, W0, b0):
-        self.W = W0.copy()
-        self.b = b0.copy()
-        self.vW = np.zeros(self.W.shape)
-        self.vb = np.zeros(self.b.shape)
-
-    def fit_step(self, x, y, learning_rate, mu, reg):
-        batch_size = x.shape[0]
-        p_hat = self.forward(x)
-        errors = p_hat - y
-        dW = _dW0(x, errors) / batch_size + reg * self.W
-        db = _db0(errors) / batch_size + reg * self.b
-        self.vW = mu * self.vW - learning_rate * dW
-        self.vb = mu * self.vb - learning_rate * db
-        self.W = self.W + self.vW
-        self.b = self.b + self.vb
-
-    def forward(self, x):
-        return softmax(_forward_log_odds(x, self.W, self.b))
-
-
-class LRfit_by_gd_with_nesterov_momentum():
-    def __init__(self, W0, b0):
-        self.W = W0.copy()
-        self.b = b0.copy()
-        self.vW = np.zeros(self.W.shape)
-        self.vb = np.zeros(self.b.shape)
-
-    def fit_step(self, x, y, learning_rate, mu, reg):
-        batch_size = x.shape[0]
-        p_hat = self.forward(x)
-        errors = p_hat - y
-        dW = _dW0(x, errors) / batch_size + reg * self.W
-        db = _db0(errors) / batch_size + reg * self.b
-        self.vW = mu * self.vW - learning_rate * dW
-        self.vb = mu * self.vb - learning_rate * db
-        self.W = self.W + mu * self.vW - learning_rate * dW
-        self.b = self.b + mu * self.vb - learning_rate * db
-
-    def forward(self, x):
-        return softmax(_forward_log_odds(x, self.W, self.b))
-
-
-class ANNfit_by_gd():
+class ANNfit():
     def __init__(self, W, b):
         self.W0 = W[0].copy()
         self.b0 = b[0].copy()
         self.W1 = W[1].copy()
         self.b1 = b[1].copy()
-
-    def fit_step(self, x, y, learning_rate, reg):
-        batch_size = x.shape[0]
-        z, p_hat = self._forward(x)
-        errors = p_hat - y
-        self.W1 = self.W1 - learning_rate * (_dW1(z, errors) / batch_size + reg * self.W1)
-        self.b1 = self.b1 - learning_rate * (_db1(errors) / batch_size + reg * self.b1)
-        self.W0 = self.W0 - learning_rate * (_dW0(x, z, errors, self.W1) / batch_size + reg * self.W0)
-        self.b0 = self.b0 - learning_rate * (_db0(z, errors, self.W1) / batch_size + reg * self.b0)
 
     def _forward(self, x):
         z1 = sigmoid(_forward_log_odds(x, self.W0, self.b0))
@@ -100,12 +47,23 @@ class ANNfit_by_gd():
         return y_hat
 
 
-class ANNfit_by_gd_with_momentum():
+class ANNfit_by_gd(ANNfit):
     def __init__(self, W, b):
-        self.W0 = W[0].copy()
-        self.b0 = b[0].copy()
-        self.W1 = W[1].copy()
-        self.b1 = b[1].copy()
+        super().__init__(W, b)
+
+    def fit_step(self, x, y, learning_rate, reg):
+        batch_size = x.shape[0]
+        z, p_hat = self._forward(x)
+        errors = p_hat - y
+        self.W1 = self.W1 - learning_rate * (_dW1(z, errors) / batch_size + reg * self.W1)
+        self.b1 = self.b1 - learning_rate * (_db1(errors) / batch_size + reg * self.b1)
+        self.W0 = self.W0 - learning_rate * (_dW0(x, z, errors, self.W1) / batch_size + reg * self.W0)
+        self.b0 = self.b0 - learning_rate * (_db0(z, errors, self.W1) / batch_size + reg * self.b0)
+
+
+class ANNfit_by_gd_with_momentum(ANNfit):
+    def __init__(self, W, b):
+        super().__init__(W, b)
         self.vW0 = np.zeros(self.W0.shape)
         self.vb0 = np.zeros(self.b0.shape)
         self.vW1 = np.zeros(self.W1.shape)
@@ -131,21 +89,10 @@ class ANNfit_by_gd_with_momentum():
         self.W1 = self.W1 + self.vW1
         self.b1 = self.b1 + self.vb1
 
-    def _forward(self, x):
-        z = sigmoid(_forward_log_odds(x, self.W0, self.b0))
-        return z, softmax(_forward_log_odds(z, self.W1, self.b1))
 
-    def forward(self, x):
-        _, y_hat = self._forward(x)
-        return y_hat
-
-
-class ANNfit_by_gd_with_nesterov_momentum():
+class ANNfit_by_gd_with_nesterov_momentum(ANNfit):
     def __init__(self, W, b):
-        self.W0 = W[0].copy()
-        self.b0 = b[0].copy()
-        self.W1 = W[1].copy()
-        self.b1 = b[1].copy()
+        super().__init__(W, b)
         self.vW0 = np.zeros(self.W0.shape)
         self.vb0 = np.zeros(self.b0.shape)
         self.vW1 = np.zeros(self.W1.shape)
@@ -171,13 +118,65 @@ class ANNfit_by_gd_with_nesterov_momentum():
         self.W1 = self.W1 + self.vW1 - learning_rate * dW1
         self.b1 = self.b1 + self.vb1 - learning_rate * db1
 
-    def _forward(self, x):
-        z = sigmoid(_forward_log_odds(x, self.W0, self.b0))
-        return z, softmax(_forward_log_odds(z, self.W1, self.b1))
 
-    def forward(self, x):
-        _, y_hat = self._forward(x)
-        return y_hat
+class ANNfit_by_adagrad(ANNfit):
+    def __init__(self, W, b):
+        super().__init__(W, b)
+        self.cacheW0 = np.ones(self.W0.shape)
+        self.cacheb0 = np.ones(self.b0.shape)
+        self.cacheW1 = np.ones(self.W1.shape)
+        self.cacheb1 = np.ones(self.b1.shape)
+        self.eps = 1e-10
+
+    def fit_step(self, x, y, learning_rate, reg):
+        batch_size = x.shape[0]
+        z, p_hat = self._forward(x)
+        errors = p_hat - y
+
+        dW1 = _dW1(z, errors) / batch_size + reg * self.W1
+        db1 = _db1(errors) / batch_size + reg * self.b1
+        dW0 = _dW0(x, z, errors, self.W1) / batch_size + reg * self.W0
+        db0 = _db0(z, errors, self.W1) / batch_size + reg * self.b0
+
+        self.cacheW1 = self.cacheW1 + dW1 * dW1
+        self.cacheb1 = self.cacheb1 + db1 * db1
+        self.cacheW0 = self.cacheW0 + dW0 * dW0
+        self.cacheb0 = self.cacheb0 + db0 * db0
+
+        self.W1 = self.W1 - learning_rate * dW1 / np.sqrt(self.cacheW1 + self.eps)
+        self.b1 = self.b1 - learning_rate * db1 / np.sqrt(self.cacheb1 + self.eps)
+        self.W0 = self.W0 - learning_rate * dW0 / np.sqrt(self.cacheW0 + self.eps)
+        self.b0 = self.b0 - learning_rate * db0 / np.sqrt(self.cacheb0 + self.eps)
+
+
+class ANNfit_by_RMSprop(ANNfit):
+    def __init__(self, W, b):
+        super().__init__(W, b)
+        self.cacheW0 = np.ones(self.W0.shape)
+        self.cacheb0 = np.ones(self.b0.shape)
+        self.cacheW1 = np.ones(self.W1.shape)
+        self.cacheb1 = np.ones(self.b1.shape)
+        self.eps = 1e-10
+
+    def fit_step(self, x, y, learning_rate, decay_rate, reg):
+        batch_size = x.shape[0]
+        z, p_hat = self._forward(x)
+        errors = p_hat - y
+
+        dW1 = _dW1(z, errors) / batch_size + reg * self.W1
+        db1 = _db1(errors) / batch_size + reg * self.b1
+        dW0 = _dW0(x, z, errors, self.W1) / batch_size + reg * self.W0
+        db0 = _db0(z, errors, self.W1) / batch_size + reg * self.b0
+
+        self.cacheW1 = decay_rate * self.cacheW1 + (1 - decay_rate) * dW1 * dW1
+        self.cacheb1 = decay_rate * self.cacheb1 + (1 - decay_rate) * db1 * db1
+        self.cacheW0 = decay_rate * self.cacheW0 + (1 - decay_rate) * dW0 * dW0
+        self.cacheb0 = decay_rate * self.cacheb0 + (1 - decay_rate) * db0 * db0
+
+        self.W1 = self.W1 - learning_rate * dW1 / np.sqrt(self.cacheW1 + self.eps)
+        self.b1 = self.b1 - learning_rate * db1 / np.sqrt(self.cacheb1 + self.eps)
+        self.W0 = self.W0 - learning_rate * dW0 / np.sqrt(self.cacheW0 + self.eps)
+        self.b0 = self.b0 - learning_rate * db0 / np.sqrt(self.cacheb0 + self.eps)
 
 
 def history_report(history, title=None, columns_to_report=['loss_train', 'loss_test', 'acc_train', 'acc_test']):
@@ -286,7 +285,7 @@ def fit_gd_with_momentum(Xtrain, Xtest, Ytrain, Ytest, W0, b0, n_epochs, n_batch
 
 
 def fit_gd_with_nesterov_momentum(Xtrain, Xtest, Ytrain, Ytest, W0, b0, n_epochs, n_batches, learning_rate, mu, reg,
-                     calc_history_step=None, logging_step=None, calc_history_avg_time=None, calc_history_max_time=None):
+                     calc_history_step=None, logging_step=None):
     print('Train parameters of ANN with minibatch gradient descent with nesterov momentum ...')
     print(f'fit_gd - W0.mean()={W0[0].mean()}, W0.std()={W0[0].std()}')
     print(f'fit_gd - b0.mean()={b0[0].mean()}, b0.std()={b0[0].std()}')
@@ -296,7 +295,6 @@ def fit_gd_with_nesterov_momentum(Xtrain, Xtest, Ytrain, Ytest, W0, b0, n_epochs
 
     history = []
     iter_count = 0
-    stopped = False
     ann = ANNfit_by_gd_with_nesterov_momentum(W0, b0)
     for epoch in range(n_epochs):
         Xtrain_shuffled, Ytrain_shuffled = shuffle(Xtrain, Ytrain)
@@ -307,6 +305,66 @@ def fit_gd_with_nesterov_momentum(Xtrain, Xtest, Ytrain, Ytest, W0, b0, n_epochs
             idx_to = (batch + 1) * n_samples
             Xbatch, Ybatch = Xtrain_shuffled[idx_from:idx_to], Ytrain_shuffled[idx_from:idx_to]
             ann.fit_step(Xbatch, Ybatch, learning_rate, mu, reg)
+
+            if (iter_count % calc_history_step) == 0:
+                history.append(_calc_history(iter_count, Xbatch, Xtest, Ybatch, Ytest, ann))
+            iter_count += 1
+    history = pd.DataFrame(history, columns=['t', 'loss_train', 'loss_test', 'acc_train', 'acc_test'])
+    print(f'fit_minibatch_gd numer of steps: {iter_count}')
+    return ann, history
+
+
+def fit_adagrad(Xtrain, Xtest, Ytrain, Ytest, W0, b0, n_epochs, n_batches, learning_rate, reg,
+                     calc_history_step=None, logging_step=None):
+    print('Train parameters of ANN with minibatch gradient descent with nesterov momentum ...')
+    print(f'fit_gd - W0.mean()={W0[0].mean()}, W0.std()={W0[0].std()}')
+    print(f'fit_gd - b0.mean()={b0[0].mean()}, b0.std()={b0[0].std()}')
+    print(f'fit_gd - W1.mean()={W0[1].mean()}, W0.std()={W0[1].std()}')
+    print(f'fit_gd - b1.mean()={b0[1].mean()}, b0.std()={b0[1].std()}')
+    n_samples = np.ceil(Xtrain.shape[0] / n_batches).astype(int)
+
+    history = []
+    iter_count = 0
+    ann = ANNfit_by_adagrad(W0, b0)
+    for epoch in range(n_epochs):
+        Xtrain_shuffled, Ytrain_shuffled = shuffle(Xtrain, Ytrain)
+        for batch in range(n_batches):
+            if logging_step is not None and (iter_count % logging_step) == 0:
+                print(f'----------epoch {epoch} - batch {batch}----------')
+            idx_from = batch * n_samples
+            idx_to = (batch + 1) * n_samples
+            Xbatch, Ybatch = Xtrain_shuffled[idx_from:idx_to], Ytrain_shuffled[idx_from:idx_to]
+            ann.fit_step(Xbatch, Ybatch, learning_rate, reg)
+
+            if (iter_count % calc_history_step) == 0:
+                history.append(_calc_history(iter_count, Xbatch, Xtest, Ybatch, Ytest, ann))
+            iter_count += 1
+    history = pd.DataFrame(history, columns=['t', 'loss_train', 'loss_test', 'acc_train', 'acc_test'])
+    print(f'fit_minibatch_gd numer of steps: {iter_count}')
+    return ann, history
+
+
+def fit_rmsprop(Xtrain, Xtest, Ytrain, Ytest, W0, b0, n_epochs, n_batches, learning_rate, decay_rate, reg,
+                     calc_history_step=None, logging_step=None):
+    print('Train parameters of ANN with minibatch gradient descent with nesterov momentum ...')
+    print(f'fit_gd - W0.mean()={W0[0].mean()}, W0.std()={W0[0].std()}')
+    print(f'fit_gd - b0.mean()={b0[0].mean()}, b0.std()={b0[0].std()}')
+    print(f'fit_gd - W1.mean()={W0[1].mean()}, W0.std()={W0[1].std()}')
+    print(f'fit_gd - b1.mean()={b0[1].mean()}, b0.std()={b0[1].std()}')
+    n_samples = np.ceil(Xtrain.shape[0] / n_batches).astype(int)
+
+    history = []
+    iter_count = 0
+    ann = ANNfit_by_RMSprop(W0, b0)
+    for epoch in range(n_epochs):
+        Xtrain_shuffled, Ytrain_shuffled = shuffle(Xtrain, Ytrain)
+        for batch in range(n_batches):
+            if logging_step is not None and (iter_count % logging_step) == 0:
+                print(f'----------epoch {epoch} - batch {batch}----------')
+            idx_from = batch * n_samples
+            idx_to = (batch + 1) * n_samples
+            Xbatch, Ybatch = Xtrain_shuffled[idx_from:idx_to], Ytrain_shuffled[idx_from:idx_to]
+            ann.fit_step(Xbatch, Ybatch, learning_rate, decay_rate, reg)
 
             if (iter_count % calc_history_step) == 0:
                 history.append(_calc_history(iter_count, Xbatch, Xtest, Ybatch, Ytest, ann))
@@ -337,8 +395,8 @@ if __name__ == '__main__':
     W1 = np.random.randn(M, K) / np.sqrt(M)
     b1 = np.zeros(K)
     regularization = 0.01
-    n_epochs = 20
-    report_step = 50
+    n_epochs = 50
+    report_step = 5
     n_batches = 50
 
     histories = []
@@ -348,34 +406,36 @@ if __name__ == '__main__':
                                                      n_epochs=n_epochs, n_batches=n_batches,
                                                      learning_rate=0.001, reg=regularization,
                                                      calc_history_step=report_step, logging_step=report_step)
-    print(f'final - W0.mean()={mgd_model.W0.mean()}, W0.std()={mgd_model.W0.std()}')
-    print(f'final - b0.mean()={mgd_model.b0.mean()}, b0.std()={mgd_model.b0.std()}')
-    print(f'final - W1.mean()={mgd_model.W1.mean()}, W1.std()={mgd_model.W1.std()}')
-    print(f'final - b1.mean()={mgd_model.b1.mean()}, b1.std()={mgd_model.b1.std()}')
     histories.append(mgd_fit_history)
     titles.append('Minibatch gradient descent')
 
     mgdm_model, mgdm_fit_history = fit_gd_with_momentum(Xtrain, Xtest, Ytrain, Ytest, [W0, W1], [b0, b1],
                                                      n_epochs=n_epochs, n_batches=n_batches,
-                                                     learning_rate=0.0001, mu=0.9, reg=regularization,
+                                                     learning_rate=0.001, mu=0.9, reg=regularization,
                                                      calc_history_step=report_step, logging_step=report_step)
-    print(f'final - W0.mean()={mgdm_model.W0.mean()}, W0.std()={mgdm_model.W0.std()}')
-    print(f'final - b0.mean()={mgdm_model.b0.mean()}, b0.std()={mgdm_model.b0.std()}')
-    print(f'final - W1.mean()={mgdm_model.W1.mean()}, W1.std()={mgdm_model.W1.std()}')
-    print(f'final - b1.mean()={mgdm_model.b1.mean()}, b1.std()={mgdm_model.b1.std()}')
     histories.append(mgdm_fit_history)
     titles.append('Minibatch gradient descent with momentum')
 
     mgdnm_model, mgdnm_fit_history = fit_gd_with_nesterov_momentum(Xtrain, Xtest, Ytrain, Ytest, [W0, W1], [b0, b1],
                                                      n_epochs=n_epochs, n_batches=n_batches,
-                                                     learning_rate=0.0001, mu=0.9, reg=regularization,
+                                                     learning_rate=0.001, mu=0.9, reg=regularization,
                                                      calc_history_step=report_step, logging_step=report_step)
-    print(f'final - W0.mean()={mgdnm_model.W0.mean()}, W0.std()={mgdnm_model.W0.std()}')
-    print(f'final - b0.mean()={mgdnm_model.b0.mean()}, b0.std()={mgdnm_model.b0.std()}')
-    print(f'final - W1.mean()={mgdnm_model.W1.mean()}, W1.std()={mgdnm_model.W1.std()}')
-    print(f'final - b1.mean()={mgdnm_model.b1.mean()}, b1.std()={mgdnm_model.b1.std()}')
     histories.append(mgdnm_fit_history)
     titles.append('Minibatch gradient descent with nesterov momentum')
+
+    ada_model, ada_fit_history = fit_adagrad(Xtrain, Xtest, Ytrain, Ytest, [W0, W1], [b0, b1],
+                                                     n_epochs=n_epochs, n_batches=n_batches,
+                                                     learning_rate=0.001, reg=regularization,
+                                                     calc_history_step=report_step, logging_step=report_step)
+    histories.append(ada_fit_history)
+    titles.append('AdaGrad')
+
+    rms_model, rms_fit_history = fit_rmsprop(Xtrain, Xtest, Ytrain, Ytest, [W0, W1], [b0, b1],
+                                                     n_epochs=n_epochs, n_batches=n_batches,
+                                                     learning_rate=0.001, decay_rate=0.999, reg=regularization,
+                                                     calc_history_step=report_step, logging_step=report_step)
+    histories.append(rms_fit_history)
+    titles.append('RMSProp')
 
     cols = ['loss_train', 'loss_test', 'acc_train', 'acc_test']
     report_histories(histories, titles, columns_to_report=cols, columns_to_plot=cols)
