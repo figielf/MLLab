fb3abc55e3d1362e047b41abb5dff67a5030cd15
figielf <mfig99@gmail.com>
2022-07-01 20:17:44 +0200

    add restricted bolzman machine

--- mllib/rbm.py
+++ mllib/rbm.py
@@ -15,40 +15,41 @@ class rbm:
         self.M = M
         self.id = str(id)
         self.W = None
-        self.bh = None
-        self.bo = None
+        self.c = None
+        self.b = None
         self._session = None
         self._initialize_variables(init_weights)
         self._prepare_operations()
 
     def _initialize_variables(self, init_weights):
         if init_weights is None:
-            #W_init = (np.random.randn(D, self.M) / np.sqrt(D)).astype(np.float32)
-            W_init = (np.random.randn(self.D, self.M)).astype(np.float32)
-            bh_init = np.zeros(self.M).astype(np.float32)
-            bo_init = np.zeros(self.D).astype(np.float32)
+            W_init = (np.random.randn(self.D, self.M) * np.sqrt(2.0 / self.M)).astype(np.float32)
+            #W_init = (np.random.randn(self.D, self.M)).astype(np.float32)
+            c_init = np.zeros(self.M).astype(np.float32)
+            b_init = np.zeros(self.D).astype(np.float32)
         else:
-            W_init, bh_init, bo_init = init_weights
+            W_init, c_init, b_init = init_weights
 
         self.W = tf.Variable(W_init)
-        self.bh = tf.Variable(bh_init)
-        self.bo = tf.Variable(bo_init)
+        self.c = tf.Variable(c_init)
+        self.b = tf.Variable(b_init)
 
     def _prepare_operations(self):
         self._x_input = tf.compat.v1.placeholder(tf.float32, shape=(None, self.D))
-        self._z = self.forward_hidden(self._x_input) # for transform() later
-        self._x_hat = self.forward_output(self._x_input)
+        self._h = self.prob_h_eq_1_given_v(self._x_input)
+        h_random_sample = self.bernoulli_sample(self._h)
+        self._v = self.prob_v_eq_1_given_h(h_random_sample)
+        v_prim_random_sample = self.bernoulli_sample(self._v)
+
+        L = tf.reduce_mean(self.free_energy(self._x_input)) - tf.reduce_mean(self.free_energy(v_prim_random_sample))
+        self._train_op = tf.compat.v1.train.AdamOptimizer(1e-2).minimize(L)
 
         x_hat_logits = self.forward_logits(self._x_input)
         self._cost = tf.reduce_mean(
             input_tensor=tf.nn.sigmoid_cross_entropy_with_logits(
                 labels=self._x_input,
-                logits=x_hat_logits,
-            )
-        )
-
-        self._train_op = tf.compat.v1.train.AdamOptimizer(1e-1).minimize(self._cost)
-        # self.train_op = tf.train.MomentumOptimizer(1e-3, momentum=0.9).minimize(self.cost)
+                logits=x_hat_logits
+            ))
 
     def set_session(self, session):
         self._session = session
@@ -66,18 +67,26 @@ class rbm:
                 batch = X[j * batch_size : (j * batch_size + batch_size)]
                 _, c = self._session.run((self._train_op, self._cost), feed_dict={self._x_input: batch})
                 if j % 100 == 0:
-                    print(f'autoencoder:{self.id} - cost after epoch:{i}, batch:{j} - {c}')
+                    print(f'rmb:{self.id} - cost after epoch:{i}, batch:{j} - {c}')
                 history.append(c)
         return history
 
-    def forward_hidden(self, x):
-        _, D = x.shape
+    def prob_h_eq_1_given_v(self, v):
+        _, D = v.shape
         assert self.D == D
-        return tf.nn.sigmoid(tf.matmul(x, self.W) + self.bh)
+        return tf.nn.sigmoid(tf.matmul(v, self.W) + self.c)
+
+    def prob_v_eq_1_given_h(self, h):
+        _, M = h.shape
+        assert self.M == M
+        return tf.nn.sigmoid(tf.matmul(h, tf.transpose(self.W)) + self.b)
+
+    def forward_hidden(self, x):
+        return self.prob_h_eq_1_given_v(x)
 
     def forward_logits(self, x):
         z = self.forward_hidden(x)
-        return tf.matmul(z, tf.transpose(a=self.W)) + self.bo
+        return tf.matmul(z, tf.transpose(self.W)) + self.b
 
     def forward_output(self, x):
         return tf.nn.sigmoid(self.forward_logits(x))
@@ -85,19 +94,25 @@ class rbm:
     def transform(self, X):
         _, D = X.shape
         assert self.D == D
-        return self._session.run(self._z, feed_dict={self._x_input: X})
+        return self._session.run(self._h, feed_dict={self._x_input: X})
 
     def predict(self, X):
         _, D = X.shape
         assert self.D == D
         return self._session.run(self._x_hat, feed_dict={self._x_input: X})
 
-    def sample_h_given_v(self):
-        return 1
-
-    def sample_v_given_h(self):
-        return 2
+    def bernoulli_sample(self, p):
+        u = tf.random.uniform(tf.shape(p))
+        return tf.cast(u < p, dtype=tf.float32)
 
     def free_energy(self, v):
-        return v + 1
+        term_1 = -tf.matmul(v, tf.reshape(self.b, (self.D, 1)))
+
+        # calculate: log(1 + tf.exp(tf.matmul(v, self.W) + self.c)),
+        term_2 = -tf.reduce_sum(
+            input_tensor=tf.nn.softplus(tf.matmul(v, self.W) + self.c),
+            axis=1
+        )
+
+        return tf.reshape(term_1, (-1,)) + term_2
 
